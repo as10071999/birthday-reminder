b487f29aa85f6e12c7347df21b82609c115a195a
Aryan <as10071999@gmail.com>
2021-02-17 08:41:06 +0530

    🚧 Evaluated Age

--- frontend/src/components/View.jsx
+++ frontend/src/components/View.jsx
@@ -6,14 +6,17 @@ function View() {
   const [people, setPeople] = useState(data);
   const [isFetching, setIsFetching] = useState(true);
   console.log("People Data", people);
-  useEffect(async () => {
-    setIsFetching(true);
-    let url = "http://127.0.0.1:8000/people/";
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      let url = "http://127.0.0.1:8000/people/";
 
-    const response = await axios.get(url);
-    console.log(response.data);
-    setPeople(response.data);
-    setIsFetching(false);
+      const response = await axios.get(url);
+      console.log(response.data);
+      setPeople([...response.data]);
+      setIsFetching(false);
+    }
+    fetchData();
   }, []);
   return (
     <>
@@ -24,35 +27,39 @@ function View() {
             <Paper elevation={5} style={{ width: "250px" }}>
               <Grid container direction="column" spacing={2}>
                 {people.map((person, i) => {
+                  const getAge = () => {
+                    var parts = person.dob.match(/(\d+)/g);
+                    // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+                    let birthday = new Date(parts[0], parts[1] - 1, parts[2]); // months are 0-based
+
+                    var ageDifMs = Date.now() - birthday.getTime();
+                    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+                    return Math.abs(ageDate.getUTCFullYear() - 1970);
+                  };
+                  let age = getAge();
                   return (
-                    <>
-                      <Grid item container direction="row">
-                        <Grid item xs></Grid>
-                        <Grid item style={{ backgroundColor: "white" }} xs={2}>
-                          <Avatar src={person.image} />
-                        </Grid>
-                        <Grid item xs></Grid>
-                        <Grid
-                          item
-                          container
-                          direction="column"
-                          style={{ backgroundColor: "white" }}
-                          xs={7}
-                        >
-                          <Grid item>
-                            <Typography variant="body1" component="body1">
-                              {person.name}
-                            </Typography>
-                          </Grid>
-                          <Grid item>
-                            <Typography variant="body2" component="body2">
-                              {/* {person.age} */}Hello
-                            </Typography>
-                          </Grid>
-                        </Grid>
-                        <Grid item xs></Grid>
+                    <Grid item container direction="row" key={person.id}>
+                      <Grid item xs></Grid>
+                      <Grid item style={{ backgroundColor: "white" }} xs={2}>
+                        <Avatar src={person.image} />
                       </Grid>
-                    </>
+                      <Grid item xs></Grid>
+                      <Grid
+                        item
+                        container
+                        direction="column"
+                        style={{ backgroundColor: "white" }}
+                        xs={7}
+                      >
+                        <Grid item>
+                          <Typography variant="body1">{person.name}</Typography>
+                        </Grid>
+                        <Grid item>
+                          <Typography variant="body2">{age}</Typography>
+                        </Grid>
+                      </Grid>
+                      <Grid item xs></Grid>
+                    </Grid>
                   );
                 })}
               </Grid>
