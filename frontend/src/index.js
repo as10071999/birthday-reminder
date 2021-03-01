e9c8d4deccfd9e110750cc3c0364f3d7dfa287cc
Aryan Singh <as10071999@gmail.com>
2021-03-01 12:24:13 +0530

    📦 Changed Referneces

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/Birthday/App";
 import "fontsource-roboto";
 import { CssBaseline } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
