72f52463b710f913e820621b380bbfdbe62baeaf
Aryan Singh <as10071999@gmail.com>
2021-03-02 12:26:18 +0530

    ⬆️ Material Ui Lab 🚧 Login Page

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -9,81 +9,114 @@ import {
   Checkbox,
   makeStyles,
   Container,
+  CircularProgress,
 } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
 import { Face, Fingerprint } from "@material-ui/icons";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
   margin: {
-    margin: theme.spacing.unit * 2,
+    margin: theme.spacing(2),
   },
   padding: {
-    padding: theme.spacing.unit,
+    padding: theme.spacing(1),
   },
 }));
 export default function Login() {
   const classes = useStyles();
+  let history = useHistory();
+  const isLoading = useSelector((state) => state.auth.isLoading);
+  const error = useSelector((state) => state.auth.error);
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert("Form Submitted");
+  }
   return (
     <Container maxWidth="sm">
       <Paper className={classes.padding}>
-        <div className={classes.margin}>
-          <Grid container spacing={8} alignItems="flex-end">
+        {error && <Alert severity="error">{error}</Alert>}
+        {isLoading ? (
+          <Grid container>
+            <Grid item xs></Grid>
             <Grid item>
-              <Face />
+              <CircularProgress />
             </Grid>
-            <Grid item md={true} sm={true} xs={true}>
-              <TextField
-                id="username"
-                label="Username"
-                type="email"
-                fullWidth
-                autoFocus
-                required
-              />
-            </Grid>
+            <Grid item xs></Grid>
           </Grid>
-          <Grid container spacing={8} alignItems="flex-end">
-            <Grid item>
-              <Fingerprint />
+        ) : (
+          <form className={classes.margin} onSubmit={handleSubmit}>
+            <Grid container spacing={8} alignItems="flex-end">
+              <Grid item>
+                <Face />
+              </Grid>
+              <Grid item md={true} sm={true} xs={true}>
+                <TextField
+                  id="username"
+                  label="Username"
+                  type="text"
+                  fullWidth
+                  autoFocus
+                  required
+                />
+              </Grid>
             </Grid>
-            <Grid item md={true} sm={true} xs={true}>
-              <TextField
-                id="password"
-                label="Password"
-                type="password"
-                fullWidth
-                required
-              />
+            <Grid container spacing={8} alignItems="flex-end">
+              <Grid item>
+                <Fingerprint />
+              </Grid>
+              <Grid item md={true} sm={true} xs={true}>
+                <TextField
+                  id="password"
+                  label="Password"
+                  type="password"
+                  fullWidth
+                  required
+                />
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container alignItems="center" justify="space-between">
-            <Grid item>
-              <FormControlLabel
-                control={<Checkbox color="primary" />}
-                label="Remember me"
-              />
+            <Grid container alignItems="center" justify="space-between">
+              <Grid item>
+                <FormControlLabel
+                  control={<Checkbox color="primary" />}
+                  label="Remember me"
+                />
+              </Grid>
+              <Grid item>
+                <Button
+                  disableFocusRipple
+                  disableRipple
+                  style={{ textTransform: "none" }}
+                  variant="text"
+                  color="primary"
+                >
+                  Forgot password ?
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item>
+            <Grid container justify="center" style={{ marginTop: "10px" }}>
               <Button
-                disableFocusRipple
-                disableRipple
-                style={{ textTransform: "none" }}
-                variant="text"
+                variant="outlined"
                 color="primary"
+                style={{ textTransform: "none" }}
+                type="submit"
               >
-                Forgot password ?
+                Login
+              </Button>
+              <Button
+                variant="outlined"
+                color="primary"
+                style={{ textTransform: "none" }}
+                onClick={() => {
+                  history.push("/register");
+                }}
+              >
+                SignUp
               </Button>
             </Grid>
-          </Grid>
-          <Grid container justify="center" style={{ marginTop: "10px" }}>
-            <Button
-              variant="outlined"
-              color="primary"
-              style={{ textTransform: "none" }}
-            >
-              Login
-            </Button>
-          </Grid>
-        </div>
+          </form>
+        )}
       </Paper>
     </Container>
   );
